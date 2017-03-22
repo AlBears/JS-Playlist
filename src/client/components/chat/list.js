046153168b27f3559dc191fded7080e10e7493b8
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-22 10:20:47 +0300

    chat list

--- src/client/components/chat/list.js
+++ src/client/components/chat/list.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import moment from 'moment';
+import { Observable } from 'rxjs';
 
 import { ElementComponent } from '../../lib/component';
 
@@ -12,8 +14,51 @@ export class ChatListComponent extends ElementComponent {
 	}
 
 	_onAttach() {
-		this._chat.messages$.compSubscribe(this, message => {
-			this.$element.append($(`<li />`).text(message.message));
-		});
+		Observable.merge(
+			this._chat.messages$.map(chatMessageFactory),
+			this._users.state$.map(userActionFactory),
+			this._server.status$.map(serverStatusFactory))
+			.filter(m => m)
+			.compSubscribe(this, $newElement => {
+				this.$element.append($newElement);
+				this.$element[0].scrollTop = this.$element[0].scrollHeight;
+			});
 	}
+}
+
+function serverStatusFactory({ isConnected, isReconnecting, attempt }) {
+	let statusMessage = null;
+	if (isConnected) statusMessage = "connected";
+	else if (isReconnecting) statusMessage = `reconnecting (attempt ${attempt})`;
+	else statusMessage = "gg. it's over now";
+
+	if (statusMessage == null)
+		return null;
+
+	return $(`<li class="server-status" />`).append([
+		$(`<span class="author" />`).text('system'),
+		$(`<span class="message" />`).text(statusMessage),
+		$(`<time />`).text(moment().format("h:mm:ss a"))
+	]);
+}
+
+function userActionFactory({ type, user }) {
+	if (type !== "add" && type !== "remove")
+		return null;
+
+	return $(`<li class="user-action ${type}" />`).append([
+		$(`<span class="author" />`).text(user.name).css("color", user.color),
+		$(`<span class="message" />`).text(type === "add" ? "joined" : "left"),
+		$(`<time />`).text(moment().format('h:mm:ss a'))
+	]);
+}
+
+function chatMessageFactory({ user, message, type, time }) {
+	return $(`<li class="message ${type}"/>`)
+		.data('user', user.name)
+		.append([
+			$(`<span class="author" />`).text(user.name).css("color", user.color),
+			$(`<span class="message" />`).text(message),
+			$(`<time />`).text(moment(time).format('h:mm:ss a'))
+		]);
 }
